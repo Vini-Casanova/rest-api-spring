81e303ab91255d86678ef77f9879596096ae0bb2
Vini-Casanova <dukeofgod@outlook.com>
2023-01-11 22:19:43 -0300

    feat: CRUD complete

--- api-spring/src/main/java/br/com/casanova/apispring/controllers/PersonController.java
+++ api-spring/src/main/java/br/com/casanova/apispring/controllers/PersonController.java
@@ -5,9 +5,9 @@ import br.com.casanova.apispring.services.PersonServices;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -17,8 +17,27 @@ public class PersonController {
     private PersonServices services;
 
     @RequestMapping(value = "/{id}" , method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathParam(value = "id") Long id)throws Exception{
+    public Person findById(@PathVariable Long id)throws Exception{
             return services.findById(id);
     }
+
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findById()throws Exception{
+        return services.findAll();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) throws Exception {
+        return services.update(person);
+    }
+
+    @RequestMapping(value = ("/{id}"),method = RequestMethod.DELETE)
+    public void update(@PathVariable Long id) throws Exception {
+        services.delete(id);
+    }
+
+
+
+
 
 }
