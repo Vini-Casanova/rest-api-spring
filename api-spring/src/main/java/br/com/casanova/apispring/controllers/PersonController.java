000ecf98e064ead8d6d04f52dc9c8477531f921b
Vini-Casanova <dukeofgod@outlook.com>
2023-01-16 01:06:17 -0300

    feat: support for json/xml/x-yaml request and response via header

--- api-spring/src/main/java/br/com/casanova/apispring/controllers/PersonController.java
+++ api-spring/src/main/java/br/com/casanova/apispring/controllers/PersonController.java
@@ -3,8 +3,8 @@ package br.com.casanova.apispring.controllers;
 import br.com.casanova.apispring.data.vo.v1.PersonVO;
 import br.com.casanova.apispring.data.vo.v2.PersonVOV2;
 import br.com.casanova.apispring.services.PersonServices;
+import br.com.casanova.apispring.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,29 +17,37 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @GetMapping(value = "/{id}" , produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(value = "/{id}" , produces = {MediaType.APPLICATION_JSON,
+                                                MediaType.APPLICATION_XML,
+                                                MediaType.APPLICATION_YML})
     public PersonVO findById(@PathVariable Long id)throws Exception{
             return services.findById(id);
     }
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public List<PersonVO> findById()throws Exception{
+    @GetMapping(produces = {MediaType.APPLICATION_JSON,
+                            MediaType.APPLICATION_XML,
+                            MediaType.APPLICATION_YML})
+    public List<PersonVO> findAll()throws Exception{
         return services.findAll();
     }
 
-    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON,
+                             MediaType.APPLICATION_XML,
+                             MediaType.APPLICATION_YML})
     public void save(@RequestBody PersonVO person)throws Exception{
         services.create(person);
     }
 
-    @PostMapping(value = "/v2",consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @PostMapping(value = "/v2",consumes = {MediaType.APPLICATION_JSON,
+                                             MediaType.APPLICATION_XML,
+                                             MediaType.APPLICATION_YML})
     public void saveV2(@RequestBody PersonVOV2 person)throws Exception{
         services.createV2(person);
     }
 
 
-    @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-                produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+                produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO update(@RequestBody PersonVO person) throws Exception {
         return services.update(person);
     }
