44a2e5ab46fcf7d3a6bbb3302cc7d6754268c62c
Vini-Casanova <dukeofgod@outlook.com>
2023-01-22 01:18:20 -0300

    feat: settings for OpenAPI Swagger documentation

---
+++ api-spring/src/main/java/br/com/casanova/apispring/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package br.com.casanova.apispring.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Spring")
+                        .version("v1")
+                        .description("A course API for learning Spring concepts")
+                        .termsOfService("")
+                        .license(new License().name("Apache 2.0").url("")));
+    }
+
+}
